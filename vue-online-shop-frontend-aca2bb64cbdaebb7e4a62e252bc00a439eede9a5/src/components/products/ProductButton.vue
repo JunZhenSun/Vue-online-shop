183234e010913d39f9765599f1ec6cdc04d40565
JunZhenSun <1135187847@qq.com>
2021-11-14 19:16:48 +0800

    Initial commit

---
+++ vue-online-shop-frontend-aca2bb64cbdaebb7e4a62e252bc00a439eede9a5/src/components/products/ProductButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <el-button
+      v-if="isAdding"
+      @click="addToCart"
+      type="text"
+      size="small">
+      加入购物车
+    </el-button>
+    <el-button
+      v-else
+      @click="removeFromCart(id)"
+      type="text"
+      size="small">
+      从购物车移除
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  computed: {
+    product() {
+      // eslint-disable-next-line no-underscore-dangle
+      return this.$store.getters.allProducts.find(product => product._id === this.id);
+    },
+    isAdding() {
+      let isAdding = true;
+      // eslint-disable-next-line array-callback-return
+      this.cart.map((product) => {
+        // eslint-disable-next-line no-underscore-dangle
+        if (product._id === this.product._id) {
+          isAdding = false;
+        }
+      });
+
+      return isAdding;
+    },
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
+  methods: {
+    addToCart() {
+      this.$store.commit('ADD_TO_CART', {
+        product: this.product,
+      });
+    },
+    removeFromCart(productId) {
+      this.$store.commit('REMOVE_FROM_CART', {
+        productId,
+      });
+    },
+  },
+};
+</script>
---
+++ vue-online-shop-frontend-aca2bb64cbdaebb7e4a62e252bc00a439eede9a5/src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div className="title">
+      <h1>In Stock</h1>
+    </div>
+    <product-list></product-list>
+  </div>
+</template>
+
+<script>
+import ProductList from '@/components/products/ProductList';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+  components: {
+    'product-list': ProductList,
+  },
+};
+</script>
